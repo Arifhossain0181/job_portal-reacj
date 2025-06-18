17132c6e22d8bc184d51eb5f376992f6f418a612
Arifhossain0181 <arifhossain01816047075@gmail.com>
2025-06-18 19:52:12 +0600

    your message

--- src/Pages/Addjobs/Addjobs.jsx
+++ src/Pages/Addjobs/Addjobs.jsx
@@ -2,9 +2,31 @@ import React from 'react';
 
 const Addjobs = () => {
     return (
-        <div>
-            <h2 className='text-3xl '> Post a new jobs</h2>
-        </div>
+        <div className="hero bg-base-200 min-h-screen">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+    <div className="text-center lg:text-left">
+      <h1 className="text-5xl font-bold">Login now!</h1>
+      <p className="py-6">
+        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+        quasi. In deleniti eaque aut repudiandae et a id nisi.
+      </p>
+    </div>
+    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+      <div className="card-body">
+        {/*job title */}
+        <form className="fieldset">
+          <label className="label">Job title</label>
+          <input type="email" className="input" placeholder="Email" name='title'/>
+          {/*job location */}
+          <label className="label">Job Location</label>
+          <input type="text" className="input" placeholder="location" name='location' />
+        
+          <button className="btn btn-neutral mt-4">Login</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
     );
 };
 
